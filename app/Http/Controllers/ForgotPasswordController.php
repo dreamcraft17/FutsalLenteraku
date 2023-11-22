6d35dc6ba0407d3bb4ce156abb35651eea26b4ac
Dozer Napitupulu <dozerfernando.napitupulu@mattel.com>
2023-11-22 23:28:37 +0700

    anjay

---
+++ app/Http/Controllers/ForgotPasswordController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+
+class ForgotPasswordController extends Controller
+{
+    //
+
+    use SendsPasswordResetEmails;
+}
